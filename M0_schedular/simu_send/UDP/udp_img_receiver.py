277487bdb30b5b5806128b67b282802afc17b84e
Nautilus1993 <2271275281@qq.com>
2023-12-08 20:34:04 +0800

    fix: UDP lose packet

--- M0_schedular/simu_send/UDP/udp_img_receiver.py
+++ M0_schedular/simu_send/UDP/udp_img_receiver.py
@@ -12,30 +12,27 @@ RECV_PORT = 8089
 # 数据分片大小
 CHUNK_SIZE = 1024
 HEADER_SIZE = 10 # TODO：暂时只加包序号调通代码
+UDP_FORMAT = "!HII1024s"    # UDP包格式
 
-# timeout设置
-TIMEOUT = 10
-
-# 图像大小，应该是固定的大小 2k * 2k
-IMAGE_SIZE = 2207156 # 6621914 6M.png
+# 图像大小
+IMAGE_SIZE = 6621914 # 图片大小 6M.png
 
 # 收到图片存放位置
 img_dir = 'received_images/'
 
+# 解析UDP包，返回包头信息和数据有效部分
 def unpack_udp_packet(udp_packet):
-    # Define the packet format
-    packet_format = "!HII1024s"
-    chunk_length, chunk_sum, chunk_seq, image_chunk = struct.unpack(packet_format, udp_packet)
-    # print(f"Unpack UDP packet {chunk_seq}/{chunk_sum}, image_chunk length is {chunk_length}")
-    return chunk_sum, chunk_seq, image_chunk[:chunk_length]
+    effect_len, \
+    chunk_sum, \
+    chunk_seq, \
+    image_chunk \
+    = struct.unpack(UDP_FORMAT, udp_packet)
+    return chunk_sum, chunk_seq, image_chunk[:effect_len]
 
-
+# 用当前时间命名图像数据
 def generate_image_name():
-    # 获取当前时间
     current_time = datetime.datetime.now()
-    # 将当前时间转换为指定格式的字符串
     time_string = current_time.strftime("%Y-%m-%d_%H-%M-%S")
-    # 构建文件名
     file_name = f"file_{time_string}.png"
     return file_name
 
@@ -51,56 +48,51 @@ def timer(func):
         return result
     return wrapper
 
-# 将接收到的数据写入文件，发送给redis
+# 将图片数据存入文件
+# TODO: 将接收到的图片数据和元信息写入文件，并发送给redis
 def process_image(image_data):
-    # TODO: add image meta data like timestamp
     filename = os.path.join(img_dir, generate_image_name())
     with open(filename, 'wb') as file:
         file.write(image_data)
 
 # @timer
 def receive_image(image_size, buffer_size):
-    # 接收UDP帧
+    # 以包序号为key存储UDP包中的有效数据
     received_packets = {}
+    received_chunks = 0
 
     while True:
-        # 接收分片数据
         udp_packet, addr = sock.recvfrom(buffer_size)
-        # TODO:加入解析UDP的逻辑
+        # TODO:完善解析UDP的逻辑
         chunk_sum, chunk_seq, image_chunk = unpack_udp_packet(udp_packet)
-        # print(f"Received packet {chunk_seq}/{chunk_sum}: {len(image_chunk)} bytes")
+        # print(f"接收到编号 {chunk_seq} 的分片，分片大小 {len(image_chunk)} bytes")
 
-        # 如果是第一帧，则清空buffer 重新累加
+        # 如果收到第一帧时字典和计数器非空，说明上一张图片有丢包
         if chunk_seq == 0:
             if(len(received_packets) != 0):
-                print("收到第一帧数据，但是当前buffer中还有数据，清空buffer重新接收")
+                print("丢包:!收到第一帧数据，字典非空，清空received_packet重新接收")
             received_packets = {}
-
-        # 如果是最后一帧，需要判断当前是否已经收齐了所有的数据包
-        # if chunk_seq == (chunk_sum - 1):
-        #     print(chunk_seq)
-        #     if len(received_packets) != chunk_sum:
-        #         print(f"已经接收到最后一包，但还未收全，应收到 {chunk_sum}, 已收到 {len(received_packets)}")
-        #         return
+            received_chunks = 0
 
         # 将当前Packet加入
         if chunk_seq not in received_packets:
             received_packets[chunk_seq] = image_chunk
+            received_chunks += 1
 
-        # Check if all packets have been received
-        if len(received_packets) == chunk_sum:
-            # Concatenate the packets in the correct order
-            print("   =========   ") 
-            print("received_packages = " + str(len(received_packets)))
-            sorted_packets = [received_packets[i] for i in range(chunk_sum)]
-            image_data = b''.join(sorted_packets)
+        # 如果是最后一帧，判断目前是否收到所有的包；若完整收到一幅图，组包存储为图片文件
+        if chunk_seq == (chunk_sum - 1):
+            print(" === 接收到最后一包 === ")
+            if len(received_packets) == chunk_sum:
+                print("已接收到所有分片共 " + str(len(received_packets)) + " 个")
+                # Concatenate the packets in the correct order
+                sorted_packets = [received_packets[i] for i in range(chunk_sum)]
+                image_data = b''.join(sorted_packets)
+                received_packets = {}
+                process_image(image_data)
+            else:
+                print(f"还未收全，应收到 {chunk_sum}, 已收到 {len(received_packets)}")
+                return 
 
-            # Reset the received packets dictionary
-            received_packets = {}
-
-            # Process the complete image data
-            process_image(image_data)
-    
     # 关闭套接字
     # sock.close()
 
@@ -108,13 +100,10 @@ def receive_image(image_size, buffer_size):
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    
 # 绑定IP地址和端口号
 sock.bind((RECV_IP, RECV_PORT))
-# sock.settimeout(TIMEOUT)
 
 while(True):
     try:
-        # 收到图像数据包的第一帧
-        # meta_data = sock.recv(CHUNK_SIZE) 
-        # print("received meta_data")  
+        # buffer_size: UDP包的大小，每次接收定长的UDP包
         buffer_size = HEADER_SIZE + CHUNK_SIZE
         receive_image(IMAGE_SIZE, buffer_size)
 
@@ -126,4 +115,3 @@ while(True):
             # 其他错误，需要处理
             print('Error:', e)
             break
-        
--- M0_schedular/simu_send/UDP/udp_img_sender.py
+++ M0_schedular/simu_send/UDP/udp_img_sender.py
@@ -6,32 +6,28 @@ import time
 import logging
 
 """
-Header 
-Byte[0:1]   data length
-Byte[2]     data receiver
-Byte[3]     data sender
-Byte[4:7]   timestamp of image in second
-Byte[8:11]  timestamp of image in millisecond
-Byte[12]    chunk number
-Byte[13]    package sequence number
-Byte[14:14 + 1024] data
+Header: 暂时只加包序号调通代码
+Byte[0:1]   有效数据长度,最后一包分片有效长度与图片大小有关 
+Byte[2:5]   包总数
+Byte[6:9]   包序号
+Byte[10:N]  数据域固定1024bytes
 """
 
 # 发送端的IP地址和端口号
+# TODO:改为配置文件
 SEND_IP = '192.168.31.17'
 SEND_PORT = 8089
-IMAGE_SIZE = 2207156 # 6621914 6M.png
-CHUNK_SIZE = 1024    # 图片分片长度
-HEADER_SIZE = 8 # TODO：暂时只加报序号调通代码
+IMAGE_SIZE = 6621914        # 图片大小 6M.png
+CHUNK_SIZE = 1024           # 图片分片长度
+UDP_FORMAT = "!HII1024s"    # UDP包格式
 
 # 在图片数据帧头包裹一层header
-def pack_udp_frame(chunk_sum, chunk_seq, image_chunk):
-    packet_format = "!HII1024s"
-    print(f"Pack UDP packet {chunk_seq}/{chunk_sum}, image_chunk length is {len(image_chunk)}")
-    udp_frame = struct.pack(packet_format, len(image_chunk), chunk_sum, chunk_seq, image_chunk)
-    return udp_frame
-
-
+def pack_udp_packet(chunk_sum, chunk_seq, image_chunk):
+    # 补全数据域为1024定长
+    byte_array = bytearray(b'0' * 1024)
+    byte_array[:len(image_chunk)] = image_chunk
+    udp_packet = struct.pack(UDP_FORMAT, len(image_chunk), chunk_sum, chunk_seq, bytes(byte_array))
+    return udp_packet
 
 def send_image(filename, server_ip, server_port, chunk_size):
     # 创建UDP套接字
@@ -46,20 +42,20 @@ def send_image(filename, server_ip, server_port, chunk_size):
     chunk_sum = (image_size + chunk_size - 1) // chunk_size
 
     # 发送图像分片
-    send_chunks = 0
+    sent_chunks = 0
     for i in range(chunk_sum):
         start = i * chunk_size
         end = min((i + 1) * chunk_size, image_size)
         image_chunk = image_data[start:end]
         
         # 发送UDP帧
-        udp_frame = pack_udp_frame(chunk_sum, i, image_chunk)
-        # print(f"Sent packet {i}/{chunk_sum}: {len(image_chunk)} bytes")
-        sock.sendto(udp_frame, (server_ip, server_port))
-        send_chunks += 1
+        udp_packet = pack_udp_packet(chunk_sum, i, image_chunk)
+        sock.sendto(udp_packet, (server_ip, server_port))
+        # print(f"发送第 {i} / {chunk_sum} 个分片，分片大小 {len(image_chunk)} bytes")
+        sent_chunks += 1
     
     # 输出发送图片结束时的日志：分片数量，文件大小
-    print("已发送分片数 " + str(send_chunks))
+    print("已发送分片数: " + str(sent_chunks))
         
     #关闭文件
     file.close()
@@ -68,7 +64,7 @@ def send_image(filename, server_ip, server_port, chunk_size):
     sock.close()
 
 # 图像文件路径
-img_file = './new.png'
+img_file = './6M.png'
 
 # 模拟1s发一张图
 while(True):
@@ -76,5 +72,3 @@ while(True):
     # for img_name in img_list:
     send_image(img_file, SEND_IP, SEND_PORT, CHUNK_SIZE)
     time.sleep(3)
-# send_image(img_file, SEND_IP, SEND_PORT, CHUNK_SIZE)
-
