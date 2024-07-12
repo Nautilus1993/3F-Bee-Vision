fd14d08331cbcbfb65d2fbed701f12f51a14410d
zzyoazzy <zzyoazzy@163.com>
2024-07-12 12:06:08 +0800

    add M3, M3 docker compose

--- M0_schedular/simu_send/redis/img_sender2.py
+++ M0_schedular/simu_send/redis/img_sender2.py
@@ -3,6 +3,7 @@ import os
 import time
 import base64
 import cv2
+import random
 
 # config
 img_dir = 'images/'
@@ -10,6 +11,20 @@ conn = redis.Redis(host='127.0.0.1', port=6379)
 
 # get image name
 img_list = sorted(os.listdir(img_dir))
+
+
+def generate_image_filename():
+    # 获取当前时间的时间戳
+    current_time = time.time()
+    
+    # 将时间戳分成秒和毫秒
+    seconds = int(current_time)
+    milliseconds = int((current_time - seconds) * 1000)
+    
+    # 生成文件名
+    filename = f"image_{seconds}_{milliseconds}_{random.randint(1000, 9999)}.bmp"
+    return filename
+
 
 # send
 for img_name in img_list:
@@ -20,9 +35,9 @@ for img_name in img_list:
     encoded_img = base64.b64encode(img_data).decode('utf-8')    # serialize
 
     message = {
-        'name': img_name,
-        'win_size': (1024, 1024),
-        'window': [1024, 0],
+        'name': generate_image_filename(),
+        'win_size': (2048, 2048),
+        'window': [0, 0],
         'data': encoded_img
     }
 
